3872377b3f9236f477932e592e3c5cf7eba557fa
jennielai <jennie.t.lai001@gmail.com>
2022-01-13 14:26:57 -0500

    updated json

--- models.py
+++ models.py
@@ -9,12 +9,28 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String)
 
+    def to_json(self):
+        return {
+            "id": self.id,
+            "first": self.first,
+            "last": self.last,
+            "email": self.email
+        }
+        
 class Meal(db.Model):
     __tablename__ = 'meals'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     meal_name = db.Column(db.String)
     date = db.Column(db.DateTime)
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "user_id": self.user.id,
+            "meal_name": self.meal_name,
+            "date": self.date,
+        }
 
 class Nutrition(db.Model):
     __tablename__ = 'nutritions'
@@ -25,10 +41,25 @@ class Nutrition(db.Model):
     carbohydrate = db.Column(db.String)
     fat = db.Column(db.String)
     calories = db.Column(db.String)
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "user_id": self.user.id,
+            "protein": self.protein,
+            "carbohydrate": self.carbohydrate,
+            "fat": self.fat,
+            "calories": self.calories
+        }
 
 class Meal_Nutrition(db.Model):
     __tablename__ = 'meals_nutritions'
     id = db.Column(db.Integer, primary_key=True)
     meal_id = db.Column(db.Integer, db.ForeignKey('meals.id'))
     nutrition_id = db.Column(db.Integer, db.ForeignKey('nutritions.id'))
-
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "nutrition_id ": self.nutrition_id,
+        }
